ef040a20b852a64797694b1df0f11d47c146f5a1
Akash Kumar <akashkumar8442@gmail.com>
2019-06-05 10:12:51 +0530

    Sucessfully done

---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,20 @@
+$(function(){
+	switch(menu){
+		case 'About Us':
+		$('#about').addClass('active');
+		break;
+		
+		case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+		
+		case 'All Products':
+		$('#listProduct').addClass('active');
+		break;
+		
+		default:
+		$('#listProduct').addClass('active');
+		$('#a_'+menu).addClass('active');
+		break;
+	}
+});
